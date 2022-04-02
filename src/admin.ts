68eaefbf6804fad2b466a13db36f7b2e35fff7f1
Jakub <jakub@cubepotato.eu>
2022-04-02 18:44:44 +0200

    ++

--- src/admin.ts
+++ src/admin.ts
@@ -20,7 +20,7 @@ app.post('/', upload.single('file'), async (req, res) => {
 
     console.log('new image template loaded');
 
-    res.redirect(req.path);
+    res.redirect('./');
 });
 
 const port = process.env.ADMIN_PORT || 3001;
