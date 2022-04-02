973790d4124ebd56a4f9f634ca60a48aed192506
Jakub <jakub@cubepotato.eu>
2022-04-02 20:06:31 +0200

    ++

--- src/admin.ts
+++ src/admin.ts
@@ -11,7 +11,7 @@ const app = express();
 // app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send(`Postęp: ${Math.round(progress * 100)}`);
+    res.send(`Postęp: ${Math.round(progress * 100)}%`);
 });
 
 app.get('/current.png', (req, res) => {
