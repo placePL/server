46f047889b49bf256d3d1c62bafc36759417a9ab
Jakub <jakub@cubepotato.eu>
2022-04-03 16:04:04 +0200

    ++

--- src/placeCanvas.ts
+++ src/placeCanvas.ts
@@ -33,8 +33,8 @@ export async function getPixelsAt(x: number, y: number, w: number, h: number): P
     busy = true;
 
     page.goto('https://www.reddit.com/r/place/?cx=500&cy=500&px=460');
-    await page.waitForSelector('.moeaZEzC0AbAvmDwN22Ma');
-    await page.click('.moeaZEzC0AbAvmDwN22Ma');
+    // await page.waitForSelector('.moeaZEzC0AbAvmDwN22Ma');
+    // await page.click('.moeaZEzC0AbAvmDwN22Ma');
 
     const elementHandle = await page.waitForSelector('iframe.Q-OBKuePQXXm3LGhGfv3k');
     const frame = (await elementHandle!.contentFrame())!;
