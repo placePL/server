e50c6a7c571edcf936f49f55218fdffd9fd07603
Jakub <jakub@cubepotato.eu>
2022-04-03 18:13:18 +0200

    ++

--- src/placeCanvas.ts
+++ src/placeCanvas.ts
@@ -31,6 +31,7 @@ export async function getPixelsAt(x: number, y: number, w: number, h: number): P
         }
 
         page = await browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36');
 
 
         busy = true;
